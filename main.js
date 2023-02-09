07d9aef677e6bcc51aab9497d75cec05d8bcf720
cynthia nweke <nwekecynthia2015@gmail.com>
2023-02-09 20:43:47 +0100

    work

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 
     window.addEventListener("scroll", function(){
-        const navbar = document.querySelector("nav-bar");
-        navbar.classList.toggle('sticky', window.scrollY > 0);
+        var header = document.querySelector("header");
+        header.classList.toggle('sticky', window.scrollY > 0);
         });
+
+
+        var ourbody=document.querySelector(".ourpagebody")
+    // ourbody.style.background="black"
+    var hamburger=document.querySelector(".hamburger-container")
+
+    // console.log(hamburger)
+
+    function addandremove(){
+    hamburger.classList.toggle('showburger')
+    }
+    var mobilelinkscontainer=document.querySelector('.mobile-links-container')
+
+    function addandremove(){
+        hamburger.classList.toggle('showburger')
+        mobilelinkscontainer.classList.toggle('showmobilelinks')
+    }
+
     
